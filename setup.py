6518d15a622104e6a8c914f0cce1712d98f1b6df
Armaan Cheema <71794534+armaancheema484@users.noreply.github.com>
2023-06-21 12:35:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 setup(
-  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  name = 'DockerImageBuilder',         # How you named your package folder (MyLib)
+  packages = ['DockerImageBuilder'],   # Chose the same as "name"
+  version = '1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  description = 'Backup lib for MIDAS project',   # Give a short description about your library
+  author = 'Armaan Cheema',                   # Type in your name
+  author_email = 'armaancheema484@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/armaancheema484/MIDAS',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/armaancheema484/MIDAS/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['BUILDER', 'IMAGE', 'DOCKER'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
           'beautifulsoup4',
@@ -19,9 +19,10 @@ setup(
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.11',
   ],
 )
