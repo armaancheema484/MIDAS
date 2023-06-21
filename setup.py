6bbd73ee47e77383c0990a4debe32d2ecacb1124
Armaan Cheema <71794534+armaancheema484@users.noreply.github.com>
2023-06-21 12:42:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,7 @@ setup(
   url = 'https://github.com/armaancheema484/MIDAS',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/armaancheema484/MIDAS/archive/refs/tags/v_1.tar.gz',    # I explain this later on
   keywords = ['BUILDER', 'IMAGE', 'DOCKER'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  install_requires=[            # list all pip packages
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
