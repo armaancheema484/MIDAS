ae22236b045bac78b6b1852670b1058d0e7cd9a2
Armaan Cheema <71794534+armaancheema484@users.noreply.github.com>
2023-06-21 12:39:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Armaan Cheema',                   # Type in your name
   author_email = 'armaancheema484@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/armaancheema484/MIDAS',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/armaancheema484/MIDAS/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/armaancheema484/MIDAS/archive/refs/tags/v_1.tar.gz',    # I explain this later on
   keywords = ['BUILDER', 'IMAGE', 'DOCKER'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
